93b4e7bd1cab7e47282691fb9f4d5e902d27bf3f
guscmiranda <gcmiranda2005@gmail.com>
2025-06-30 18:23:19 -0300

    Python: entendendo o uso de *args e **kwargs em funções e métodos

---
+++ src/04-funcoes/aula03.py
@@ -0,0 +1,41 @@
+""" Python: entendendo o uso de *args e **kwargs em funções e métodos """
+
+print("*args\n")
+
+
+def world_cup_titles(country, *args):
+
+    print('Country: ', country)
+
+    for title in args:
+        print('year: ', title)
+
+
+world_cup_titles('Brasil', '1958', '1962', '1970', '1994', '2002')
+world_cup_titles('Espanha', '2010')
+
+print("\n--------------\n")
+print("**kwargs\n")
+
+
+def calculate_price(value, **kwargs):
+    tax_percentage = kwargs.get('tax_percentage')
+    discount = kwargs.get('discount')
+    if tax_percentage:
+        value += value * (tax_percentage / 100)
+    if discount:
+        value -= discount
+    return value
+
+
+final_price = calculate_price(100.0)
+print(final_price)
+
+final_price = calculate_price(100.0, discount=5.0)
+print(final_price)
+
+final_price = calculate_price(100.0, tax_percentage=7)
+print(final_price)
+
+final_price = calculate_price(100.0, tax_percentage=7, discount=5.0)
+print(final_price)
