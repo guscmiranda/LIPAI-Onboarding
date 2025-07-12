bcbad809bbfddb062edbefa7e14709baba3358bb
guscmiranda <gcmiranda2005@gmail.com>
2025-07-11 22:18:12 -0300

    módulo desafios ex01

---
+++ src/08-modulos-e-pacotes/exercícios/moeda.py
@@ -0,0 +1,18 @@
+def aumentar(valor, p):
+    return valor + valor*p/100
+
+
+def diminuir(valor, p):
+    return valor - valor*p/100
+
+
+def dobro(valor):
+    return valor*2
+
+
+def metade(valor):
+    return valor/2
+
+
+def moeda(valor):
+    return (f"R${valor:.2f}")
